9ffd6148dc4266faee1ed4504ddcc1e880a9a023
Larry Le <larrylenw@gmail.com>
2025-04-05 09:34:49 +0700

    config theme

--- client/src/components/layouts/AdminLayout/index.tsx
+++ client/src/components/layouts/AdminLayout/index.tsx
@@ -1,11 +1,30 @@
 'use client';
 import StyledComponentsRegistry from '@/lib/antd.registry';
+import { Button } from 'antd';
+import { signOut } from "next-auth/react";
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
+
+
+  const handleSignOut = async () => {
+    try {
+      await signOut({ redirect: true });
+      console.log("Signed out successfully");
+    } catch (error) {
+      console.error("Error signing out", error);
+    }
+  };
+
+
+
+
   return (
     <html lang="en">
       <body>
         <StyledComponentsRegistry>
-            <div>header admin</div>
+          <div>
+            <p>header</p>
+            <Button onClick={() => handleSignOut()}>Logout</Button>
+          </div>
           <main>{children}</main>
         </StyledComponentsRegistry>
       </body>
